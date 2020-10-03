392552765ced184443e2cca62e950fe7a3bd8315
VladRafli <rafli.jaskandi@gmail.com>
2020-10-03 10:47:42 +0700

    Updates

---
+++ precache-manifest.ddaa17b7e10c9de605aa239621c9c48a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc1eb13e71062826f636b294798d9cb",
+    "url": "/Learning-Website/index.html"
+  },
+  {
+    "revision": "8e0b6f35f73f8994490c",
+    "url": "/Learning-Website/static/css/main.29d2726b.chunk.css"
+  },
+  {
+    "revision": "9fb5e8559fb532bc9347",
+    "url": "/Learning-Website/static/js/2.5f2a4e86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Learning-Website/static/js/2.5f2a4e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0b6f35f73f8994490c",
+    "url": "/Learning-Website/static/js/main.1c4583c2.chunk.js"
+  },
+  {
+    "revision": "fc5e961ef866dfdcd903",
+    "url": "/Learning-Website/static/js/runtime-main.f73c09f0.js"
+  },
+  {
+    "revision": "b3c1f609a98d6e5e5b6598847310cab4",
+    "url": "/Learning-Website/static/media/download.b3c1f609.png"
+  }
+]);
